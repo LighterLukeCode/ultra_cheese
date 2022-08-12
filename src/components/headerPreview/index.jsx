1d968a80d0977a17b5925f068b68f828f3534f3d
LighterLukeCode <lighterluke@gmail.com>
2022-08-12 17:17:15 +0300

    header

--- src/components/headerPreview/index.jsx
+++ src/components/headerPreview/index.jsx
@@ -8,44 +8,48 @@ import styles from "./headerPreview.module.scss";
 
 const HeaderPreview = () => {
   return (
-    <div className={styles.header__preview}>
-      <div className={styles.container}>
-        <div className={styles.header__inner}>
-          <button className={styles.header__btn_load}>Загрузить чек</button>
-          <h1 className="header__title">Нам 40 лет — вам подарки</h1>
-          <div className="header__content">
-            <ul className="header__content-list">
-              <li className="header__content-item">
-                <img className="header__content-icon" src={cart} alt="cart" />
-                <p className="header__content-text">Покупайте сыр Oltermanni</p>
-                <img className="header__content-arrow" src={arrow} alt="arrow" />
-              </li>
-              <li className="header__content-item">
-                <img className="header__content-icon" src={computer} alt="computer" />
-                <p className="header__content-text">
-                  Регистрируйте чеки на сайте в период с 1 сентября по 30 ноября 2020 года
-                </p>
-                <img className="header__content-arrow" src={arrow} alt="arrow" />
-              </li>
-              <li className="header__content-item">
-                <img className="header__content-icon" src={phone} alt="phone" />
-                <p className="header__content-text">Получайте гарантированные призы — деньги на телефон</p>
-                <img className="header__content-arrow" src={arrow} alt="arrow" />
-              </li>
-              <li className="header__content-item">
-                <img className="header__content-icon" src={placement} alt="placement" />
-                <p className="header__content-text">Участвуйте в еженедельных розыгрышах</p>
-                <img className="header__content-arrow" src={arrow} alt="arrow" />
-              </li>
-              <li className="header__content-item">
-                <img className="header__content-icon" src={ticket} alt="ticket" />
-                <p className="header__content-text">Получите возможность выиграть 400 000 ₽ на новый интерьер</p>
-              </li>
-            </ul>
+    <>
+      <div className={styles.header__preview}>
+        <div className={styles.container}>
+          <div className={styles.header__inner}>
+            <button className={styles.header__btn_load}>Загрузить чек</button>
           </div>
         </div>
       </div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.header__content}>
+          <h1 className={styles.header__title}>Нам 40 лет — вам подарки</h1>
+          <ul className="header__content-list">
+            <li className="header__content-item">
+              <img className="header__content-icon" src={cart} alt="cart" />
+              <p className="header__content-text">Покупайте сыр Oltermanni</p>
+              <img className="header__content-arrow" src={arrow} alt="arrow" />
+            </li>
+            <li className="header__content-item">
+              <img className="header__content-icon" src={computer} alt="computer" />
+              <p className="header__content-text">
+                Регистрируйте чеки на сайте в период с 1 сентября по 30 ноября 2020 года
+              </p>
+              <img className="header__content-arrow" src={arrow} alt="arrow" />
+            </li>
+            <li className="header__content-item">
+              <img className="header__content-icon" src={phone} alt="phone" />
+              <p className="header__content-text">Получайте гарантированные призы — деньги на телефон</p>
+              <img className="header__content-arrow" src={arrow} alt="arrow" />
+            </li>
+            <li className="header__content-item">
+              <img className="header__content-icon" src={placement} alt="placement" />
+              <p className="header__content-text">Участвуйте в еженедельных розыгрышах</p>
+              <img className="header__content-arrow" src={arrow} alt="arrow" />
+            </li>
+            <li className="header__content-item">
+              <img className="header__content-icon" src={ticket} alt="ticket" />
+              <p className="header__content-text">Получите возможность выиграть 400 000 ₽ на новый интерьер</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
